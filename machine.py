6d0521f01343860587028e77ba47cbc5e31147f0
root <justin.mangini@esg.global>
2021-04-10 07:12:58 -0600

    setup cure oven basicis

--- machine.py
+++ machine.py
@@ -1,6 +1,9 @@
-from odoo_automation import automation, conveyor, dispenser
+from odoo_automation import automation, automation_web, conveyor, dispenser
 import logging, time, odoorpc
 import configparser, argparse
+import RPi.GPIO as GPIO 
+GPIO.setmode(GPIO.BCM)
+import digitalio, board
 
 #setup console logger
 logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s - %(message)s",datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
@@ -8,13 +11,16 @@ logger=logging.getLogger("Peak Station")
 
 class MRP_machine(automation.MRP_Automation):
     def __init__(self, api, config):
-        super(MRP_machine, self).__init__(api, int(config['machine']['equipment_id']))
+        self.conveyor_1 = Conveyor_1("Oven Conveyor",config["conveyor_1"])
+        result = super(MRP_machine, self).__init__(api, int(config['machine']['equipment_id']),config)
 
         #init route lanes
         self.route_lanes = [MRP_Carrier_Lane_0(self.api, self)]
+        self.oven_currenttemp = 0.0
+        
         
         logger.info("Machine INIT Complete.")
-        return
+        return result
     
     def indicator_start(self, value):
         
@@ -30,29 +36,55 @@ class MRP_machine(automation.MRP_Automation):
 
     #Button inputs
     def button_start(self):
+        self.conveyor_1.start()
         return super(MRP_machine, self).button_start()
     
     def button_stop(self):
+        self.conveyor_1.stop()
         return super(MRP_machine, self).button_stop()
     
     def e_stop(self):
         #put render safe i/o here.
+        self.conveyor_1.e_stop()
         return super(MRP_machine, self).e_stop()
     
     def e_stop_reset(self):
         #put reboot i/o here
+        self.conveyor_1.e_stop_reset()
         return super(MRP_machine, self).e_stop_reset()
 
     def get_blocking_status(self):
         return super(MRP_machine, self).get_blocking_status()   
 
     def quit(self):
+        self.conveyor_1.quit()
         return super(MRP_machine, self).quit()
 
 class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
     def __init__(self, api, mrp_automation_machine):
         super(MRP_Carrier_Lane_0, self).__init__(api, mrp_automation_machine)
         self._logger = logging.getLogger("Carrier Lane 0")
+        self.config =  self.mrp_automation_machine.config["lane0"]
+        
+        self.ingress_pin = digitalio.DigitalInOut(board.D14)
+        self.ingress_pin.direction = digitalio.Direction.INPUT
+        self.ingress_pin.pull = digitalio.Pull.UP
+        
+        self.egress_pin = digitalio.DigitalInOut(board.D16)
+        self.egress_pin.direction = digitalio.Direction.INPUT
+        self.egress_pin.pull = digitalio.Pull.UP
+        
+        self.carrier_stop_pin = digitalio.DigitalInOut(board.D25)
+        self.carrier_stop_pin.direction = digitalio.Direction.OUTPUT
+        self.carrier_stop_pin.value = 0
+        
+        GPIO.setup(int(self.config["pwm_pin2"]),GPIO.OUT)
+        self.pi_pwm2 = GPIO.PWM(int(self.config["pwm_pin2"]),int(self.config["pwm_freq2"]))		
+        self.pi_pwm2.start(0)
+        
+        #install custom carrier calss into lane
+        self.carrier_class = Carrier
+        
         self._logger.info("Lane INIT Complete")
         pass
     
@@ -62,10 +94,15 @@ class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
 
     def ingress_trigger(self):
         #to be inherited by the main machine config and returns True when the product has arrived at the ingress gate.
-        return super(MRP_Carrier_Lane_0, self).ingress_trigger()
+        # if self.ingress_pin.value == False:
+        #     self._logger.info("Pallet Detected")
+        #     return super(MRP_Carrier_Lane_0, self).ingress_trigger()
+            
+        return False
 
     def process_ingress(self):
         #to be inherited by the main machine config and returns True when the product has processed through ingress and is ready for processing.
+        
         return super(MRP_Carrier_Lane_0, self).process_ingress()
     
     def process_carrier(self):
@@ -73,11 +110,40 @@ class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
         
     def process_egress(self):
         #to be inherited by the main machine config and returns True when the product has processed through egress and is clear of this machine.
+        self.pi_pwm2.ChangeDutyCycle(10)
+        if self.carrier_stop_pin.value == True:
+            return True
+            
         return super(MRP_Carrier_Lane_0, self).process_egress()
         
     def quit(self):
+        self.pi_pwm2.ChangeDutyCycle(0)
         return super(MRP_Carrier_Lane_0, self).quit()
+        
+class Conveyor_1(conveyor.Conveyor):
     
+    def __init__(self, name,config):
+        self.config = config
+        result = super(Conveyor_1,self).__init__(name)
+        GPIO.setup(int(self.config["pwm_pin"]),GPIO.OUT)
+        self.pi_pwm = GPIO.PWM(int(self.config["pwm_pin"]),int(self.config["pwm_freq"]))		
+        self.pi_pwm.start(0)
+        speed_pwm = self.config["speed_pwm"]
+        return result
+        
+    def set_speed(self, freq_offset):
+        self.pi_pwm.ChangeDutyCycle(20)
+        return True
+        
+    def quit(self):
+        self.pi_pwm.ChangeDutyCycle(0)
+        return super(Conveyor_1, self).quit()
+   
+class Carrier(automation.Carrier):
+    def __init__(self, api, mrp_automation_machine, carrier_lane):
+        return super(Carrier, self).__init__(api, mrp_automation_machine, carrier_lane)
+        
+        
 def create_odoo_api():
     #create odoo api object
     try:
@@ -113,9 +179,26 @@ if __name__ == '__main__':
     machine = MRP_machine(odoo_api, config)
     
     #uncomment for machine auto start
-    #machine.button_start()
+    machine.button_start()
     
     while 1:
         #main thread eep alive
         time.sleep(1000)
     pass
+
+#Blue Board:
+# Inputs:
+# 1 - gpio_18
+# 2 - gpio_06
+# 3 - gpio_05
+# 4 - gpio_23
+# 5 - gpio_24
+# 6 - gpio_22
+# Outputs:
+# 1 - gpio_12 (pwm)
+# 2 - gpio_13 (pwm)
+# 3 - gpio_25
+# 4 - gpio_26
+# 5 - gpio_27
+# 6 - gpio_04
+
